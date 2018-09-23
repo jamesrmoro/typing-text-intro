d2ee34445b1eb6f703433a636e4595580bb07983
James R. Moro <jamesrmoro@gmail.com>
2018-09-23 00:39:37 -0300

    Novo vídeo

--- js/dev.js
+++ js/dev.js
@@ -2,10 +2,10 @@
 
     var msg = [];
     msg[0] = "Oi";
-    msg[1] = "Gostaria que você desenvolvesse um modal em vue.js.";
-    msg[2] = "Não tem como não!!!";
-    msg[3] = "Pra hoje!!! Até as 18h consegue?";
-    msg[4] = "Valeu!";
+    msg[1] = "Eu preciso de uma tela parecida com os commits do git";
+    msg[2] = "Para cada vídeo meu no Youtube quero que mostre naqueles 'azulejinhos'";
+    msg[3] = "Tá bom, vou mandar um print da minha ideia";
+    msg[4] = "Obrigada :)";
 
 	function addZero(i) {
 	    if (i < 10) {
@@ -23,7 +23,7 @@
 	var avatar1 = '<div class="avatar"><img src="images/james.jpg"></div>';
 	var avatar2 = '<div class="avatar"><img src="images/usuario.png"></div>';
 	var text1 = '<div class="text text1"><span class="name">James</span><span class="date"> // '+time+'</span><p>';
-	var text2 = '<div class="text text2"><span class="name">João</span><span class="date"> // '+time+'</span><p>';
+	var text2 = '<div class="text text2"><span class="name">Vanessa</span><span class="date"> // '+time+'</span><p>';
 	var textend = '</p></div>';
 	var divmsgend = '</div>';
 
@@ -41,9 +41,12 @@
 	        setTimeout(function(){$('.chat-container').prepend(divmsg+text2+msg[1]+textend+avatar2+divmsgend)},3000);
 	        break;
 	    case 2:
-	        setTimeout(function(){$('.chat-container').prepend(divmsg+text2+msg[3]+textend+avatar2+divmsgend)},3000);
+	        setTimeout(function(){$('.chat-container').prepend(divmsg+text2+msg[2]+textend+avatar2+divmsgend)},3000);
 	        break;
 	    case 3:
+	        setTimeout(function(){$('.chat-container').prepend(divmsg+text2+msg[3]+textend+avatar2+divmsgend)},3000);
+	        break;
+	    case 4:
 	        setTimeout(function(){$('.chat-container').prepend(divmsg+text2+msg[4]+textend+avatar2+divmsgend)},3000);
 	        break;
 		}
